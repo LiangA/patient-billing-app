ee02f73532667add6f3d8df2078e6d7866878415
LiangA <k2358966@livemail.tw>
2021-02-15 10:47:30 -0500

    ♻️  refactor the mock database

---
+++ src/mocks/data.js
@@ -0,0 +1,15 @@
+// prettier-ignore
+export const MEDICAL_BILLING_LIST = [
+  ['CT BRAIN',        2000, 100, 'INR', 'CT'],
+  ['CT PNS',          1000, 200, 'INR', 'CT'],
+  ['MRI BRAIN',       3000, 300, 'INR', 'MRI'],
+  ['MRI PNS',         2400, 30,  '%',   'MRI'],
+  ['GLUCOSE FASTING', 130,  10,  '%',   'LAB'],
+  ['SUGAR TESTING',   300,  5,   '%',   'LAB'],
+];
+
+export const MODALITY_LIST = [
+  ['CT', 7],
+  ['MRI', 6],
+  ['LAB', Number.MAX_SAFE_INTEGER],
+];
--- src/mocks/db.js
+++ src/mocks/db.js
@@ -1,29 +1,18 @@
-const db = {};
+import { MEDICAL_BILLING_LIST, MODALITY_LIST } from './data';
 
-// prettier-ignore
-db.medicalBillingList = [
-  ['CT BRAIN',        2000, 100, 'INR', 'CT'],
-  ['CT PNS',          1000, 200, 'INR', 'CT'],
-  ['MRI BRAIN',       3000, 300, 'INR', 'MRI'],
-  ['MRI PNS',         2400, 30,  '%',   'MRI'],
-  ['GLUCOSE FASTING', 130,  10,  '%',   'LAB'],
-  ['SUGAR TESTING',   300,  5,   '%',   'LAB'],
-].map((item) => ({
-  name: item[0],
-  amount: item[1],
-  maxDiscount: item[2],
-  discountType: item[3],
-  modality: item[4],
-}));
+const db = {
+  medicalBillingList: MEDICAL_BILLING_LIST.map((item) => ({
+    name: item[0],
+    amount: item[1],
+    maxDiscount: item[2],
+    discountType: item[3],
+    modality: item[4],
+  })),
 
-// prettier-ignore
-db.modalityList = [
-  ['CT',  7],
-  ['MRI', 6],
-  ['LAB', Number.MAX_SAFE_INTEGER],
-].map((item) => ({
-  name: item[0],
-  maxSlotPerDay: item[1],
-}));
+  modalityList: MODALITY_LIST.map((item) => ({
+    name: item[0],
+    maxSlotPerDay: item[1],
+  })),
+};
 
 export default db;
