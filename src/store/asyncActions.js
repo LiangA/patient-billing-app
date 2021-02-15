c0b5738fd5c104dee1c4fe8125ce1af096d1180f
LiangA <k2358966@livemail.tw>
2021-02-14 21:40:54 -0500

    ✨ [store] add 'maxSlotPerDay' into medicalBillingList

--- src/store/asyncActions.js
+++ src/store/asyncActions.js
@@ -2,6 +2,17 @@ import * as MockAPI from '../mocks/api';
 
 export const fetchMedicalBillingList = async (dispatch) => {
   dispatch({ type: 'LOADING_MEDICAL_BILLING_LIST' });
-  const list = await MockAPI.getMedicalBillingList();
-  dispatch({ type: 'LOADED_MEDICAL_BILLING_LIST', list });
+
+  const modalityList = await MockAPI.getModalityList();
+  const modalityMap = new Map();
+  for (const modality of modalityList) {
+    modalityMap.set(modality.name, modality.maxSlotPerDay);
+  }
+
+  const medicalBillingList = await MockAPI.getMedicalBillingList();
+  for (const medicalBilling of medicalBillingList) {
+    medicalBilling.maxSlotPerDay = modalityMap.get(medicalBilling.modality);
+  }
+
+  dispatch({ type: 'LOADED_MEDICAL_BILLING_LIST', list: medicalBillingList });
 };
